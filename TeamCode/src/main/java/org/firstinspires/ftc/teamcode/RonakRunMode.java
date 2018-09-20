acbcfcafa90ac539669b743f358c880d2977b040
Ronak Bhatia <ronakbhatia06@gmail.com>
2018-09-19 19:55:28 -0700

    I programmed the servos and motors.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RonakRunMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RonakRunMode.java
@@ -9,6 +9,8 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 @TeleOp
 public class RonakRunMode extends LinearOpMode {
     private Gyroscope imu;
@@ -33,8 +35,21 @@ public class RonakRunMode extends LinearOpMode {
         while (opModeIsActive()) {
             tgtPower = -this.gamepad1.left_stick_y;
             motorTest.setPower(tgtPower);
+            // check to see if we need to move the servo.
+            if (gamepad1.y) {
+                // move to 0 degrees.
+                servoTest.setPosition(0);
+            } else if (gamepad1.x || gamepad1.b) {
+                // move to 90 degrees.
+                servoTest.setPosition(0.5);
+            } else if (gamepad1.a) {
+                // move to 180 degrees.
+                servoTest.setPosition(1);
+            }
+            telemetry.addData("Servo Position", servoTest.getPosition());
             telemetry.addData("Target Power", tgtPower);
             telemetry.addData("Motor Power", motorTest.getPower());
+            telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
