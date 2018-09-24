b496682f500add9bf4d95fd10db7941a8bba5620
Matthew Klass <matthew.brandon@pointcircle.com>
2018-09-24 13:08:25 -0700

    From Steve

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StevensRunMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StevensRunMode.java
@@ -10,11 +10,12 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 // Created by Steve
 
-@TeleOp
+@TeleOp(name="Steven Test Tank Mode")
 public class StevensRunMode extends LinearOpMode {
 
     private Gyroscope imu;
     private DcMotor LeftMotor;
+    private DcMotor RightMotor;
     private DigitalChannel digitalTouch;
     private DistanceSensor sensorColorRange;
     private Servo ServoTest;
@@ -22,22 +23,28 @@ public class StevensRunMode extends LinearOpMode {
     @Override
     public void runOpMode() {
         imu = hardwareMap.get(Gyroscope.class, "imu");
-        LeftMotor = hardwareMap.get(DcMotor.class, "LeftMotor");
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
-        ServoTest = hardwareMap.get(Servo.class, "ServoTest");
+        LeftMotor = hardwareMap.get(DcMotor.class, "leftDrive");
+        RightMotor = hardwareMap.get(DcMotor.class, "rightDrive");
+//        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+//        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+//        ServoTest = hardwareMap.get(Servo.class, "ServoTest");
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
-        double tgtPower;
+        double tgtPowerL;
+        double tgtPowerR;
         while (opModeIsActive()) {
-            tgtPower = -this.gamepad1.left_stick_y;
-            LeftMotor.setPower(tgtPower);
-            telemetry.addData("Target Power", tgtPower);
-            telemetry.addData("Motor Power", LeftMotor.getPower());
+            tgtPowerL = -this.gamepad1.left_stick_y;
+            tgtPowerR = this.gamepad1.right_stick_y;
+            LeftMotor.setPower(tgtPowerL);
+            RightMotor.setPower(tgtPowerR);
+            telemetry.addData("Left Target Power", tgtPowerL);
+            telemetry.addData("Right Target Power", tgtPowerR);
+            telemetry.addData("Left Motor Power", LeftMotor.getPower());
+            telemetry.addData("Right Motor Power", RightMotor.getPower());
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
