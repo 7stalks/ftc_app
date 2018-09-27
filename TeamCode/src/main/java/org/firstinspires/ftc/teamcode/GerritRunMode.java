e70739489d79d7b4aad3ed57a59fed828f5305e7
Gerrit Kaup <gjkaup@icloud.com>
2018-09-26 20:09:18 -0700

    7th check in (made full drive features with 1 stick reverse turns)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GerritRunMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GerritRunMode.java
@@ -1,88 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.Gyroscope;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
-@TeleOp
+@TeleOp(name=" Gerrit Drive Arcade", group="Exercises")
 public class GerritRunMode extends LinearOpMode {
-    private Gyroscope imu;
-    private DcMotor motorTest;
-    private DcMotor motorTest2;
-    private DigitalChannel digitalTouch;
-    private DistanceSensor sensorColorRange;
-    private Servo servoTest;
+    DcMotor leftDrive, rightDrive;
+    float   leftPower, rightPower, xValue, yValue;
 
+    // init button is  pressed.
     @Override
-    public void runOpMode() {
-        imu = hardwareMap.get(Gyroscope.class, "imu");
-        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
-        motorTest2 = hardwareMap.get(DcMotor.class, "motorTest2");
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
-        servoTest = hardwareMap.get(Servo.class, "servoTest");
-        telemetry.addData("servo", "Initialized"); telemetry.update();
-        // Wait for the game to start (driver presses PLAY)
+    public void runOpMode() throws InterruptedException
+    {
+        leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        rightDrive = hardwareMap.dcMotor.get("rightDrive");
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Mode", "waiting");
+        telemetry.update();
+
+        // wait for start button.
+
         waitForStart();
-        // run until the end of the match (driver presses STOP)
-        double tgtPower = 0;
-        double tgtPower2 = 0;
-        while (opModeIsActive()) {
-            tgtPower = this.gamepad1.left_stick_y;
-            tgtPower2 = -this.gamepad1.left_stick_x;
 
-            if (tgtPower!=0 || tgtPower2!=0) {
-                if (tgtPower>0) {
-                    telemetry.addData("tgtPower>0", tgtPower);
-                    motorTest.setPower(-((tgtPower+tgtPower2)/2));
-                    motorTest2.setPower(((tgtPower+tgtPower2)/2));
-                }
-                if (tgtPower<0) {
-                    telemetry.addData("tgtPower<0", tgtPower);
-                    motorTest.setPower(-((tgtPower+tgtPower)/2));
-                    motorTest2.setPower(((tgtPower+tgtPower2)/2));
-                }
-                if (tgtPower2>0) {
-                    telemetry.addData("tgtPower2>0", tgtPower2);
-                    motorTest.setPower((tgtPower+tgtPower2)/2);
-                    motorTest2.setPower((tgtPower+tgtPower2)/2);
-                }
-                if (tgtPower2<0) {
-                    telemetry.addData("tgtPower2<0", tgtPower2);
-                    motorTest.setPower((tgtPower+tgtPower2)/2);
-                    motorTest2.setPower((tgtPower+tgtPower2)/2);
-                }
-                telemetry.update();
-            } else {
-                telemetry.addData("tgtPower=0", tgtPower);
-                telemetry.addData("tgtPower2=0", tgtPower2);
-                motorTest.setPower(0);
-                motorTest2.setPower(0);
-            }
+        while (opModeIsActive())
+        {
+            yValue = gamepad1.left_stick_y;
+            xValue = gamepad1.left_stick_x;
 
-            if (gamepad1.y) {
-                servoTest.setPosition(0);
-            } else if (gamepad1.x || gamepad1.b) {
-                servoTest.setPosition(0.5);
-            } else if (gamepad1.a) {
-                servoTest.setPosition(1);
-            }
-            telemetry.addData("tgtPower>0", tgtPower);
-            telemetry.addData("tgtPower<0", tgtPower);
-            telemetry.addData("tgtPower2>0", tgtPower2);
-            telemetry.addData("tgtPower2<0", tgtPower2);
-            telemetry.addData("tgtPower=0", tgtPower);
-            telemetry.addData("tgtPower2=0", tgtPower2);
-            telemetry.addData("Servo Position", servoTest.getPosition());
-            telemetry.addData("Distance (cm)", sensorColorRange.getDistance (DistanceUnit.CM));
-            telemetry.addData("status", "Running");
+            leftPower =  yValue - xValue;
+            rightPower = yValue + xValue;
+
+            leftDrive.setPower(Range.clip(leftPower, -1.0, 1.0));
+            rightDrive.setPower(Range.clip(rightPower, -1.0, 1.0));
+
+            telemetry.addData("Mode", "running");
+            telemetry.addData("stick", "  y=" + yValue + "  x=" + xValue);
+            telemetry.addData("power", "  left=" + leftDrive + "  right=" + rightDrive);
             telemetry.update();
+
+            idle();
         }
     }
 }
