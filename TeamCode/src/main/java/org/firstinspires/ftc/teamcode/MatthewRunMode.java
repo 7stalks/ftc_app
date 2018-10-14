aecd13d948ebd083c17b9ad94f59f23381c7e683
Matthew Klass <matthew.brandon@pointcircle.com>
2018-10-14 13:53:55 -0700

    i made a chain drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MatthewRunMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MatthewRunMode.java
@@ -15,6 +15,7 @@ public class MatthewRunMode extends LinearOpMode {
     private Gyroscope imu;
     private DcMotor leftDrive;
     private DcMotor rightDrive;
+    private DcMotor chainDrive;
     private DigitalChannel digitalTouch;
     private DistanceSensor sensorColorRange;
     private Servo servoTest;
@@ -24,36 +25,40 @@ public class MatthewRunMode extends LinearOpMode {
         imu = hardwareMap.get(Gyroscope.class, "imu");
         leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
-        servoTest = hardwareMap.get(Servo.class, "servoTest");
+        chainDrive = hardwareMap. get(DcMotor.class, "chaindrive");
+        //digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+        //sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+        //servoTest = hardwareMap.get(Servo.class, "servoTest");
         telemetry.addData("Status", "Initialized"); telemetry.update();
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         // run until the end of the match (driver presses STOP)
         double tgtPowerY;
         double tgtPowerX;
+        double tgtchain;
         while (opModeIsActive()) {
             tgtPowerY = -this.gamepad1.left_stick_y;
             tgtPowerX = this.gamepad1.left_stick_x;
             leftDrive.setPower(tgtPowerY);
             rightDrive.setPower(tgtPowerX);
-            if(gamepad1.y) {
-                // move to 0 degrees.
-                servoTest.setPosition(0);
-            } else if (gamepad1.x || gamepad1.b) {
-                // move to 90 degrees.
-                servoTest.setPosition(0.5);
-            } else if (gamepad1.a) {
-                // move to 180 degrees.
-                servoTest.setPosition(1);
-            }
-            telemetry.addData("Target Power", tgtPowerY);
-            telemetry.addData("Motor Power", leftDrive.getPower());
-            telemetry.addData("Target Power2", tgtPowerX);
-            telemetry.addData("Motor Power2", rightDrive.getPower());
-            telemetry.addData("Status", "Running");
-            telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
+            tgtchain = this.gamepad1.right_stick_y;
+//            if(gamepad1.y) {
+//                // move to 0 degrees.
+//                servoTest.setPosition(0);
+//            } else if (gamepad1.x || gamepad1.b) {
+//                // move to 90 degrees.
+//                servoTest.setPosition(0.5);
+//            } else if (gamepad1.a) {
+//                // move to 180 degrees.
+//                servoTest.setPosition(1);
+//            }
+            telemetry.addData("Target PowerY", tgtPowerY);
+            telemetry.addData("Motor PowerY", leftDrive.getPower());
+            telemetry.addData("Target PowerX", tgtPowerX);
+            telemetry.addData("Motor PowerX", rightDrive.getPower());
+            telemetry.addData( "Target PowerY, tgtChain", chainDrive.getPower());
+            telemetry.addData( "Motor PowerY", chainDrive.getPower());
+//            telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
             telemetry.update();
 
 
